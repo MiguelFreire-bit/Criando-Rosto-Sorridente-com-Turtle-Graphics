97cdc98a96f820c417742e62f5be8c77f6182029
Miguel Freire <miguelsilvafreire@hotmail.com>
2023-02-20 20:22:08 -0300

    deixando código mais responsivo e menos repetitivo

--- Turtle Graphics.py
+++ Turtle Graphics.py
@@ -4,28 +4,36 @@ s = turtle.Screen()
 t = turtle.Turtle()
 t.pensize(3)
 
-#fazendo contorno do rostoG
-t.circle(100)
+def pulo (t, x, y):
+    'faz tartaruga saltar para coordenadas (x,y)'
 
-#fazendo olho esquerdo
-t.penup()
-t.goto(-35,120)
-t.pendown()
-t.dot(25)
+    t.penup()
+    t.goto(x, y)
+    t.pendown()
 
-#fazendo olho direito
-t.penup()
-t.goto(35,120)
-t.pendown()
-t.dot(25)
+def carinha (t,x, y):
+    t.pensize(3)
+    t.setheading(0)
 
-#fazendo sorriso
-t.penup()
-t.goto(-60.62,80)
-t.pendown()
-t.setheading(-60)
-t.circle(70,120)
+    pulo (t, x, y)
+    #fazendo contorno do rosto
+    t.circle(100)
 
+    #fazendo olho esquerdo
+    pulo(t,x-35,y+120)
+    t.dot(25)
+
+    #fazendo olho direito
+    pulo(t,x+35, y+120)
+    t.dot(25)
+
+    #fazendo sorriso
+    pulo(t,x-60.62,y+65)
+    t.setheading(-60)
+    t.circle(70,120)
+
+carinha(t,100,50)
+carinha(t,-120,50)
 
 u = print(input("coe"))
 
